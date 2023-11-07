473f36dd0c1d94414ba55cada9880f14490cd42f
Franck Pommereau <franck.pommereau@gmail.com>
2023-11-07 16:41:05 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,80 @@
+# A bot to generate Non-Fungible Toots
+
+NFToot is a Mastodon bot that posts a NFToot for each new follower.
+
+## FAQ
+
+A NFToot is a non-fungible toot owned by some mastodon account.
+It includes:
+ - the claim that the NFToot is owned by one account
+ - hashtags
+ - the hexadecimal representation of a genuine 4-bytes strong nonce
+ - the hexadecimal SHA1 digest of the informations above
+
+All together this makes a cryptographically validated proof of ownership.
+
+##### I have a NFToot claiming to be owned by my, do I really own it?
+
+Yes, it's literally written in the toot.
+
+##### What can I do with my NFToot?
+ 
+Mainly, own it.
+But be imaginative, after all it's your NFToot.
+
+##### Somebody boosted my NFToot, is it still mine?
+  
+Yes: it's still written in the toot.
+
+##### What does non-fungible mean?
+
+This means that your NFToot cannot be broken into smaller parts.
+Obviously, doing so would result in fragments of texts that are not your NFToots, but only parts of it.
+
+##### Is a 4-byte nonce enough to guarantee my NFToot uniqueness?
+
+There's your account also, the nonce is mainly there because its a nice ornament.
+
+##### Is SHA1 strong enough to guarantee my NFToot?
+
+SHA1 is a real and serious cryptographic tool.
+May it be broken in the future, your name is still written on the toot.
+
+##### May my NFToot be lost?
+
+If the account that generated it is closes, your NFToot will not be published anymore.
+But you may copy its content to your own computer or republish it somewhere else.
+This copies are also your NFToot, or if you prefer, they are distinct instances of the same NFToot.
+
+##### Are you serious?
+
+No.
+
+##### How long does it take to get my NFToot once I follow the bot?
+
+It depends, generating a NFToot is not that long, but getting it tooted may take some time because, you know, blockchains...
+
+##### Are NFToots stored on a blockchain?
+
+Technically, each NFToot is a block a bytes, and they are arranged as a chain of toots in the bot's timeline.
+So, yes, this is actually a blockchain.
+
+## Install
+
+Just `git clone`, `cd`, and `pip install .`
+Dependencies:
+ - [`typer[all]`](https://typer.tiangolo.com/)
+ - [`Mastodon.py`](https://github.com/halcy/Mastodon.py)
+ - Python (developed and tested with 3.12)
+
+Then run `nftoot setup` to log into you bot's account.
+Finally run `nftoot update` to check for new followers and generate the corresponding NFToots.
+The latter may run in a cron tab.
+Rate limits may cause the command to crash, but you may just rerun it later on as new followers are only recorded when their NFToot has been successfully posted.
+`nftoot update` may be run from cron, just consider that it downloads the whole list of followers each time it is run, so:
+ - if run too often, it will work for nothing and consume your requests limit
+ - if run too sparsely, it will have to generate more NFToots and may also hit rate limit
+
+## Licence
+
+NFToot software is (C) 2023 Franck Pommereau <franck.pommereau@univ-evry.fr> and released under the terms of the MIT licence, see `LICENCE.md`.
---
+++ nftoot/__init__.py
@@ -0,0 +1,169 @@
+import configparser
+import webbrowser
+import secrets
+import hashlib
+import re
+
+from pathlib import Path
+from typing import Annotated
+from importlib import metadata
+from rich.prompt import Confirm, Prompt
+from rich.console import Console
+from rich.status import Status
+from rich.progress import Progress
+from typer import Typer, Option
+from mastodon import Mastodon
+
+NFTOOT_LOG = Path("~/.config/nftoot/done.txt").expanduser()
+NFTOOT_INI = Path("~/.config/nftoot/nftoot.ini").expanduser()
+NFTOOT_TXT = """This non-fungible toot is owned by {owner} and by nobody else.
+#NFT #NFToot #crypto #blockchain
+{nonce}/{digest}"""
+
+app = Typer(context_settings={"help_option_names": ["-h", "--help"]})
+con = Console()
+
+
+@app.command(help="connect to the Mastodon account")
+def setup(
+        account: Annotated[
+            str,
+            Option(
+                "--account", "-a",
+                help="identifier for the account to be created")] = "default",
+        config: Annotated[
+            Path,
+            Option(
+                "--config", "-c",
+                metavar="PATH",
+                help="PATH to configuration file")] = NFTOOT_INI):
+    conf = configparser.ConfigParser()
+    conf.read(config)
+    if account in conf:
+        if not Confirm.ask(f"Account [blue]{account}[/] exists, overwrite?"):
+            raise typer.Exit(0)
+    instance = Prompt.ask("instance name").strip()
+    cfg = {"api_base_url": f"https://{instance}"}
+    cfg["client_id"], cfg["client_secret"] = Mastodon.create_app("nftoot", **cfg)
+    masto = Mastodon(**cfg)
+    url = masto.auth_request_url()
+    con.log(f"opening page [yellow]{url}[/]")
+    webbrowser.open(url)
+    code = Prompt.ask(f"type the code you received").strip()
+    cfg["access_token"] = masto.log_in(code=code)
+    conf[account] = cfg
+    config.parent.mkdir(exist_ok=True, parents=True)
+    with config.open("w") as out:
+        conf.write(out)
+    con.log(f"saved [yellow]{config}[/]")
+
+
+def connect(profile, config):
+    conf = configparser.ConfigParser()
+    conf.read(config)
+    if profile not in conf:
+        con.print("[red]missing account[/] run [blue]nftoot setup[/] first")
+        raise typer.Exit(2)
+    cfg = conf[profile]
+    instance = cfg["api_base_url"][8:]
+    masto = Mastodon(**cfg)
+    profile = masto.me()
+    con.log(f"logged in as [blue]{profile['acct']}@{instance}[/]")
+    return instance, masto, profile
+
+
+@app.command(help="generate NFToots for new followers")
+def update(
+        account: Annotated[
+            str,
+            Option(
+                "--account", "-a",
+                help="account to be used")] = "default",
+        dryrun: Annotated[
+            bool,
+            Option(
+                "-d/-n", "--dry/--no-dry",
+                help="no not post NFToots")] = False,
+        verbose: Annotated[
+            bool,
+            Option(
+                "-v/-q", "--verbose/--quiet",
+                help="print NFToots as they are posted")] = True,
+        config: Annotated[
+            Path,
+            Option(
+                "--config", "-c",
+                metavar="PATH",
+                help="PATH to configuration file")] = NFTOOT_INI):
+    instance, masto, profile = connect(account, config)
+    with Status(f"fetching [green]{profile['followers_count']}[/] followers"):
+        first_page = masto.account_followers(profile["id"])
+        followers = masto.fetch_remaining(first_page)
+    NFTOOT_LOG.touch()
+    done = set(at.strip() for at in NFTOOT_LOG.open())
+    new = [at if "@" in at else at + f"@{instance}"
+           for user in followers
+           if (at := user["acct"]) and at not in done]
+    with NFTOOT_LOG.open("a") as log, \
+            Progress(transient=True, console=con) as progress:
+        for at in progress.track(new, description="posting NFToots"):
+            txt = NFTOOT_TXT.format(owner=f"@{at}",
+                                    nonce=secrets.token_hex(4),
+                                    digest="{}")
+            sha = hashlib.sha1(txt.encode("utf-8", errors="ignore"))
+            txt = txt.format(sha.hexdigest())
+            if not dryrun:
+                masto.status_post(txt, visibility="unlisted")
+                log.write(f"{at}\n")
+            progress.log(f"posted NFT for [green]{at}[/]"
+                         + (f"\n[dim]{txt}[/]" if verbose else ""))
+
+
+@app.command(help="publish #faq toots")
+def faq(
+    account: Annotated[
+        str,
+        Option(
+            "--account", "-a",
+            help="account to be used")] = "default",
+    dryrun: Annotated[
+        bool,
+        Option(
+            "-d/-n", "--dry/--no-dry",
+            help="no not post NFToots")] = False,
+    verbose: Annotated[
+        bool,
+        Option(
+            "-v/-q", "--verbose/--quiet",
+            help="print NFToots as they are posted")] = True,
+    config: Annotated[
+        Path,
+        Option(
+            "--config", "-c",
+            metavar="PATH",
+            help="PATH to configuration file")] = NFTOOT_INI):
+    readme = metadata.metadata("nftoot")["Description"]
+    _, faqtext, _ = (t.strip() for t in re.split("^## .*$", readme, 2, re.M))
+    last = None
+    _, masto, _ = connect(account, config)
+    for faq in (f.strip().replace("NFToot", "#NFToot")
+                for f in re.split("^##### ", faqtext, 0, re.M)):
+        if not dryrun:
+            post = masto.status_post(faq,
+                                     visibility=("public" if last is None
+                                                 else "unlisted"),
+                                     in_reply_to_id=last)
+            if last is None:
+                masto.status_pin(post["id"])
+            last = post["id"]
+        elif last is None:
+            last = 0
+        else:
+            last += 1
+        if verbose:
+            con.log(f"posted [green]#{last}[/]"
+                    + (f"\n[dim]{faq}[/]" if verbose else ""))
+
+
+if __name__ == "__main__":
+    app()
